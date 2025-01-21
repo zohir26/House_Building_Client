34e5052deb67c48f83075bb0ab5c90bf5fbd106b
zohir <jahirraihan500@gmail.com>
2025-01-22 03:18:25 +0600

    add map using piegon map

--- src/Components/Home/Location.jsx
+++ src/Components/Home/Location.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import map from '../../assets/map 2.jpg'
+import { Map, Marker } from "pigeon-maps"
 import { TypeAnimation } from 'react-type-animation';
 const Location = () => {
     return (
@@ -7,7 +8,7 @@ const Location = () => {
             <h1 className='text-2xl font-bold text-center'>
                 How to reach the destination?
             </h1>
-            <div className='m-4 text-center container'>
+            <div className='py-4 text-center container mx-auto w-1/2'>
                 <TypeAnimation
                     sequence={[
                         "It is located in  Centre of City is set in Dhaka, 3.1 km from Stamford University Bangladesh and 3.2 km from Supreme Court of Bangladesh. Among the facilities of this property are a restaurant, room service and a 24-hour front desk, along with free WiFi. The property is non-smoking and is situated 700 metres from Notre Dame University Bangladesh.BUET is 4.3 km from the accommodation, while Jagannath University is 4.3 km from the property. Hazrat Shahjalal International Airport is 13 km away."
@@ -15,12 +16,16 @@ const Location = () => {
                     wrapper="span"
                     speed={50}
                     style={{ fontSize: '1em', display: 'inline-block' }}
-                    
+
                 />
             </div>
-            <div className='py-4 m-4'>
-                <img src={map} alt="" className='rounded-xl' />
+            <div className='min-w-screen-md px-2 m-auto rounded-xl'>
+                
+                <Map height={400} width={900} defaultCenter={[23.8041, 90.4152]} defaultZoom={11}>
+                    <Marker width={50} anchor={[23.8041, 90.4152]} />
+                </Map>
             </div>
+           
         </div>
     );
 };
